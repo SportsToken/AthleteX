82710fe32e8c50d31656ab5510ec4c3a8af8de3b
Matthew Klengson <mklengs@clemson.edu>
2020-11-04 21:19:21 -0500

    Source
    Adding just the source code

---
+++ src/fighters.js
@@ -0,0 +1,26 @@
+
+
+var fighters = [
+    {
+        id: 0,
+        name: "Khabib Nurmagomedov",
+        division: "Lightweight",
+        record: "29-0", //want to replace with dataPath
+        isOwned: false,
+        //pool: "ufc-lightweight-pool"
+        weight: "100%" //% of pool occupied
+        //dataPath: "/variables/fighterData.js"
+    },
+    {
+        id: 1,
+        name: "test fighter",
+        division: "Welterweight",
+        record: "0-69",
+        isOwned: false,
+        //pool
+        weight: "0%",
+        //dataPath
+    }
+];
+
+export default fighters;
